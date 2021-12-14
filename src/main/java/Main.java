0b94ea249517fe4eb35acc68cd978a5fe50bd31f
uukkii <i.am.newbie@hotmail.com>
2021-12-14 13:28:30 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+
+    public static final List<String> savesList = new ArrayList<>();
+
+    public void main(String[] args) {
+//
+//        GameProgress gameProgress1 = new GameProgress(100, 2, 1, 1.0);
+//        GameProgress gameProgress2 = new GameProgress(80, 5, 4, 112.5);
+//        GameProgress gameProgress3 = new GameProgress(65, 9, 9, 323.6);
+//
+//        saveGame("Games/BestGame/savegames/save1.dat", gameProgress1);
+//        saveGame("Games/BestGame/savegames/save2.dat", gameProgress2);
+//        saveGame("Games/BestGame/savegames/save3.dat", gameProgress3);
+//
+//        zipFiles("Games/BestGame/savegames/saves.zip", savesList);
+//
+//        delete("Games/BestGame/savegames/save1.dat");
+//        delete("Games/BestGame/savegames/save2.dat");
+        delete("Games/BestGame/savegames/save4.dat");
+    }
+
+    public void saveGame(String dir, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(dir);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+            savesList.add(dir);
+            oos.flush();
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public void zipFiles(String dir, List<String> savesList) {
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(dir))) {
+            for (String save : savesList) {
+                FileInputStream fis = new FileInputStream(save);
+                ZipEntry entry = new ZipEntry(save.substring(25));
+                zos.putNextEntry(entry);
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer);
+                zos.write(buffer);
+                fis.close();
+                zos.closeEntry();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void delete(String dir) {
+        try {
+            Files.delete(Paths.get(dir));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/MainTests.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTests {
+    private static Main sut;
+
+    private static final String time = LocalDateTime.now()
+            .format(DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm:ss "));
+
+    @BeforeEach
+    public void init() {
+        System.out.println(time + "- test started");
+    }
+
+    @BeforeAll
+    public static void started() {
+        System.out.println(time + "- tests started");
+        sut = new Main();
+    }
+
+    @AfterEach
+    public void finished() {
+        System.out.println(time + "- test finished");
+    }
+
+    @AfterAll
+    public static void finishedAll() {
+        System.out.println(time + "- tests finished");
+    }
+
+    @Test
+    public void testDeleteThrow() {
+        String dir = "Games/BestGame/savegames/save4.dat";
+        var expected = IOException.class;
+        assertThrows(expected,
+                () -> sut.delete(dir));
+    }
+
+    @Test
+    public void testZipFiles() {
+        List<String> savesList = new ArrayList<>();
+        sut.zipFiles("Games/BestGame/savegames/saves.zip", savesList);
+        boolean result = Files.exists(Path.of("Games/BestGame/savegames/saves.zip"));
+
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("source")
+    public void testSaveDoesNotThrow(String dir, GameProgress gameProgress) {
+        assertDoesNotThrow(
+                () -> sut.saveGame(dir, gameProgress));
+    }
+
+    private static Stream<Arguments> source() {
+        return Stream.of(Arguments.of("Games/BestGame/savegames/save1.dat", new GameProgress(100, 2, 1, 1.0),
+                Arguments.of("Games/BestGame/savegames/save2.dat", new GameProgress(80, 5, 4, 112.5))));
+    }
+
+}
